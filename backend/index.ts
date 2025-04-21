bc8353fd51a8e85bd4f0b876077f95fb13613930
etai89 <senioretai@gmail.com>
2025-04-21 21:51:25 +0300

    cors again

--- backend/index.ts
+++ backend/index.ts
@@ -23,17 +23,18 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(bodyParser.json())
-
-app.use(cors({
+const corsOptions = {
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
   credentials: true
-}))
+}
 
+app.use(cors(corsOptions))
+app.options('*', cors(corsOptions)) // 👈 מאפשר תגובה לבקשות OPTIONS (preflight)
+
+app.use(bodyParser.json())
 app.use('/uploads', express.static('uploads'))
-
 
 // Init DB
 initDB()
