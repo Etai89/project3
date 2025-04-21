f77a842046f6b1a9354c3984da0e42a7b4b201e9
etai89 <senioretai@gmail.com>
2025-04-21 21:42:39 +0300

    update docker compose6

--- backend/index.ts
+++ backend/index.ts
@@ -24,7 +24,10 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 app.use('/uploads', express.static('uploads'))
 
 
